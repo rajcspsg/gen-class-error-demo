cabc8d34707db814e961fb2ebac7b8a97895fb51
Rajkumar Natarajan <rajkumar.natarajan@ekata.com>
2023-08-11 09:31:16 -0700

    apply suggestion

--- src/main/myorg/plugin/orc_output.clj
+++ src/main/myorg/plugin/orc_output.clj
@@ -1,14 +1,10 @@
 (ns myorg.plugin.orc-output
   (:import
    (myorg.helpers FsInputStream)
-   (java.nio.file Path Paths)
-   org.apache.hadoop.conf.Configuration
-   (org.apache.orc OrcFile)
    (org.apache.hadoop.fs FileSystem FSDataInputStream)))
 
 (set! *warn-on-reflection* true)
 
-(def dummy-path (Paths/get "dummy"))
 
 (defn input-stream-filesystem [buffered-bytes]
   (proxy [FileSystem] []
