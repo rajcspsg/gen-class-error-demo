caca5ca6ef2011c13fcae7040c4a377877d12b14
Rajkumar Natarajan <rajkumar.natarajan@ekata.com>
2023-08-11 09:24:18 -0700

    apply suggestion

--- src/main/myorg/plugin/orc_output.clj
+++ src/main/myorg/plugin/orc_output.clj
@@ -1,37 +1,16 @@
 (ns myorg.plugin.orc-output
   (:import
-   [myorg.helpers.fs-input-stream FsInputStream]
-   java.io.ByteArrayOutputStream
-   java.lang.UnsupportedOperationException
-   java.nio.ByteBuffer
-   [java.net URL]
-   [java.io File]
-   [java.nio.file Path]
+   (myorg.helpers FsInputStream)
+   (java.nio.file Path Paths)
    org.apache.hadoop.conf.Configuration
-   [org.apache.orc OrcFile]
-   [org.apache.hadoop.fs FileSystem FileSystem$Statistics FSDataInputStream FSDataOutputStream]))
+   (org.apache.orc OrcFile)
+   (org.apache.hadoop.fs FileSystem FSDataInputStream)))
 
 (set! *warn-on-reflection* true)
 
-(defn to-path
-  [x]
-  {:post (instance? Path %)}
-  (cond
-    (instance? URL x) (Path. (.toURI ^URL x))
-    (instance? File x) (Path. (.getPath ^File x))
-    (string? x)                (Path. ^String x)
-    (instance? Path x)         x))
-
-(def dummy-path (to-path "dummy"))
-
-(def orc-header-length (count OrcFile/MAGIC))
+(def dummy-path (Paths/get "dummy"))
 
 (defn input-stream-filesystem [buffered-bytes]
   (proxy [FileSystem] []
     (open [_]
       (FSDataInputStream. (FsInputStream. buffered-bytes)))))
-
-(defn create-orc-reader [buffered-bytes]
-  (OrcFile/createReader dummy-path (doto (OrcFile/readerOptions (Configuration.))
-                                     (.maxLength (count buffered-bytes))
-                                     (.filesystem (input-stream-filesystem buffered-bytes)))))
